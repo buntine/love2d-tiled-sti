056935966bb84d701559920dc4501a13af73ffe8
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-05-31 20:30:52 +1000

    Started adding tiled map

--- main.lua
+++ main.lua
@@ -1,10 +1,13 @@
 local sti = require("lib/sti")
 
 function love.load()
+    map = sti.new("assets/maps/level")
 end
 
 function love.draw()
+    map:draw()
 end
 
 function love.update(dt)
+    map:update(dt)
 end
