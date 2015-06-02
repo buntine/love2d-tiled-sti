9ace298c8537fd53ffc755288ec80ec6a4c63d5e
Andrew Buntine Of Doom <bunts@hhd.com.au>
2015-06-02 23:08:05 +1000

    Started testing anim8

--- main.lua
+++ main.lua
@@ -3,6 +3,22 @@ local anim8 = require("lib/anim8")
 
 function love.load()
     map = sti.new("assets/maps/level")
+    heroSprites = love.graphics.newImage("assets/spritesheets/pimple.gif")
+    heroGrid = anim8.newGrid(65, 70, 590, 85)
+
+    player = {
+      spritesheet = heroSprites,
+      x = 60,
+      y = 100,
+      speed = 50,
+      animations = {
+        still = anim8.newAnimation(heroGrid(1, '1-4'), 0.5),
+        left = anim8.newAnimation(heroGrid(2, '1-5'), 0.2),
+        right = anim8.newAnimation(heroGrid(2, '1-5'), 0.2)
+      },
+    }
+    player.animation = player.animations.still
+
     love.graphics.setBackgroundColor(127, 117, 137)
 end
 
